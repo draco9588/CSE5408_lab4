4b61f2ce4ecf3834bb88c2f68c0a7c580c4c38b3
Ricardo Torres <72730333+draco9588@users.noreply.github.com>
2021-03-29 17:25:13 -0700

    Update lab4_main.py

--- lab4_main.py
+++ lab4_main.py
@@ -27,3 +27,64 @@ if prime > 1:
         print(prime, ": is a prime number.")
 else:
     print(prime, ": is a prime number.")
+    # -*- coding: utf-8 -*-
+
+"""
+Created on Tue Mar  7 11:55:27 2021
+CSE 5408 lab 4 problem 3 section c
+convert 12 hour to 24 hour time
+First a time in 12 hour will be initialized and saved 
+then converted to 24 hour time.
+@author: rjtor
+"""
+
+def get_time():
+    from time import localtime
+    timeInTwelve = localtime()
+    return timeInTwelve
+
+def convert_time(givenTime): 
+    from time import strftime
+    result = strftime(" %H:%M:%S", givenTime )
+    print(result)
+
+
+print("print time in 12 hour mode.")
+xTime = get_time()
+print(xTime)
+
+print("print time in 24 hour mode.")
+convert_time(xTime)
+"""
+Created on Tue Mar  9 12:50:00 2021
+CSE 5408 lab 4 problem 3 section d
+This is to calculate the fibonacci sum with the given user input
+To calculate sum formula will find all fibonacci numbers then add them up
+There are more cost effective ways but this way was simplist to understand
+@author: rjtor
+"""
+
+def cal_sum(num) : 
+    if (num <= 0) : 
+        return 0
+   
+    fibNum =[0] * (num+1) 
+    fibNum[1] = 1
+   
+    # Initialize result 
+    sm = fibNum[0] + fibNum[1] 
+   
+    # Add remaining terms 
+    for i in range(2,num+1) : 
+        fibNum[i] = fibNum[i-1] + fibNum[i-2] 
+        sm = sm + fibNum[i] 
+          
+    return sm 
+
+#test
+
+print("Fibonacci sum calculator:")
+user_input = int(input("Enter the number you wished sumed: "))
+result = (cal_sum(user_input))
+print("Here is the result:")
+print(result)
